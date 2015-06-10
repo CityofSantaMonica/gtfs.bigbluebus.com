07360d477771c8203e089c60b11f613648288525
kevinsmgov <kevin.hobson@smgov.net>
2015-06-10 13:41:23 -0700

    basic shapes export into geojson

--- App_Data/jobs/continuous/extractgtfs/run.py
+++ App_Data/jobs/continuous/extractgtfs/run.py
@@ -21,6 +21,7 @@ while True:
             schedule = transitfeed.Schedule()
             trip = transitfeed.Trip()
             route = transitfeed.Route()
+            #shape = transitfeed.Shape()
 
             schedule.Load(currentzippath)
             tripList = schedule.GetTripList()
@@ -30,6 +31,15 @@ while True:
                 x = {"trip_id" : trip.trip_id, "direction_id" : trip.direction_id, "route_id" : trip.route_id, "route_short_name" : route.route_short_name, "route_long_name" : route.route_long_name, "route_color" : route.route_color}
                 forJson[trip.trip_id] = x
             json.dump(forJson, open("/home/site/wwwroot/gtfs/trips.json","w"))
+            shapeList = schedule.GetShapeList()
+            forJson = {"type": "FeatureCollection","crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } } }
+            features = []
+            for shape in shapeList:
+                feature = {"type": "Feature", "properties": { "shape_id": shape.shape_id }, "geometry" : { "type": "LineString","coordinates": [[point[1], point[0]] for point in shape.points] } }
+                features.append(feature)
+                forJson["features"] = features
+            json.dump(forJson, open("/home/site/wwwroot/gis/shapes.geojson","w"))
+
         previous_mtime = currentzipstat.st_mtime
     else:
         sleep(60)
