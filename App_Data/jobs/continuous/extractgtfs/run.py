90aba0173910315f68d4d45569bf9c7674520d65
kevinsmgov <kevin.hobson@smgov.net>
2015-06-10 14:53:53 -0700

    added route information to shape

--- App_Data/jobs/continuous/extractgtfs/run.py
+++ App_Data/jobs/continuous/extractgtfs/run.py
@@ -1,4 +1,6 @@
+import itertools
 import json
+import operator
 import os
 import sys
 from time import sleep
@@ -23,6 +25,7 @@ while True:
             route = transitfeed.Route()
             #shape = transitfeed.Shape()
 
+            #export trips for live map
             schedule.Load(currentzippath)
             tripList = schedule.GetTripList()
             forJson = {}
@@ -31,11 +34,17 @@ while True:
                 x = {"trip_id" : trip.trip_id, "direction_id" : trip.direction_id, "route_id" : trip.route_id, "route_short_name" : route.route_short_name, "route_long_name" : route.route_long_name, "route_color" : route.route_color}
                 forJson[trip.trip_id] = x
             json.dump(forJson, open("/home/site/wwwroot/gtfs/trips.json","w"))
+
+            #export shapes with route information
             shapeList = schedule.GetShapeList()
             forJson = {"type": "FeatureCollection","crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } } }
             features = []
-            for shape in shapeList:
-                feature = {"type": "Feature", "properties": { "shape_id": shape.shape_id }, "geometry" : { "type": "LineString","coordinates": [[point[1], point[0]] for point in shape.points] } }
+            sortedTrips = sorted(tripList, key=lambda item: item.shape_id)
+            for shape_id, trips in itertools.groupby(sortedTrips, key=lambda item: item.shape_id):
+                trip = next(trips)
+                route = schedule.GetRoute(trip.route_id)
+                shape = schedule.GetShape(shape_id)
+                feature = {"type": "Feature", "properties": { "shape_id": shape.shape_id, "direction_id":trip.direction_id, "route_short_name" : route.route_short_name, "route_long_name" : route.route_long_name, "route_color" : route.route_color }, "geometry" : { "type": "LineString","coordinates": [[point[1], point[0]] for point in shape.points] } }
                 features.append(feature)
                 forJson["features"] = features
             json.dump(forJson, open("/home/site/wwwroot/gis/shapes.geojson","w"))
