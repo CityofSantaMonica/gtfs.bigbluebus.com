59c9fdd4bdb753ad1406f6f9f25a1e2bc72f6988
kevinsmgov <kevin.hobson@smgov.net>
2015-06-09 19:08:47 -0700

    Python web job to extract updated GTFS file

---
+++ App_Data/jobs/continuous/extractgtfs/run.py
@@ -0,0 +1,36 @@
+import json
+import os
+import sys
+from time import sleep
+import zipfile
+
+import transitfeed
+
+currentzippath = "/home/site/wwwroot/current.zip"
+previous_mtime = 0
+
+while True:
+    currentzipstat = os.stat(currentzippath)
+    if previous_mtime != currentzipstat.st_mtime:
+        print "GTFS:" + str(currentzipstat.st_mtime)
+
+        if zipfile.is_zipfile(currentzippath):
+            currentzip = zipfile.ZipFile(currentzippath,'r')
+            currentzip.extractall("/home/site/wwwroot/gtfs")
+
+            schedule = transitfeed.Schedule()
+            trip = transitfeed.Trip()
+            route = transitfeed.Route()
+
+            schedule.Load(currentzippath)
+            tripList = schedule.GetTripList()
+            forJson = {}
+            for trip in tripList:
+                route = schedule.GetRoute(trip.route_id)
+                x = {"trip_id" : trip.trip_id, "direction_id" : trip.direction_id, "route_id" : trip.route_id, "route_short_name" : route.route_short_name, "route_long_name" : route.route_long_name, "route_color" : route.route_color}
+                forJson[trip.trip_id] = x
+            json.dump(forJson, open("/home/site/wwwroot/gtfs/trips.json","w"))
+        previous_mtime = currentzipstat.st_mtime
+    else:
+        sleep(60);
+    
---
+++ App_Data/jobs/continuous/extractgtfs/transitfeed/feedinfo.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python2.5
+
+# Copyright (C) 2011 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import transitfeed
+
+class FeedInfo(transitfeed.GtfsObjectBase):
+  """Model and validation for feed_info.txt."""
+
+  _REQUIRED_FIELD_NAMES = ["feed_publisher_name", "feed_publisher_url",
+                           "feed_lang"]
+  _FIELD_NAMES = _REQUIRED_FIELD_NAMES + ["feed_start_date", "feed_end_date",
+                                          "feed_version"]
+  _DEPRECATED_FIELD_NAMES = [('feed_valid_from', 'feed_start_date'),
+                             ('feed_valid_until', 'feed_end_date'),
+                             ('feed_timezone', None)]
+  _TABLE_NAME = 'feed_info'
+
+  def __init__(self, field_dict=None):
+    self._schedule = None
+    if field_dict:
+      self.__dict__.update(field_dict)
+
+  def ValidateFeedInfoLang(self, problems):
+    return not transitfeed.ValidateLanguageCode(self.feed_lang, 'feed_lang',
+                                                problems)
+
+  def ValidateFeedInfoPublisherUrl(self, problems):
+    return not transitfeed.ValidateURL(self.feed_publisher_url,
+                                       'feed_publisher_url', problems)
+
+  def ValidateDates(self, problems):
+    # Both validity dates are currently set to optional, thus they don't have
+    # to be provided and it's currently OK to provide one but not the other.
+    start_date_valid = transitfeed.ValidateDate(self.feed_start_date,
+                                               'feed_start_date', problems)
+
+    end_date_valid = transitfeed.ValidateDate(self.feed_end_date,
+                                                'feed_end_date', problems)
+
+    if (start_date_valid and end_date_valid and
+        self.feed_end_date < self.feed_start_date):
+        problems.InvalidValue('feed_end_date', self.feed_end_date,
+                              'feed_end_date %s is earlier than '
+                              'feed_start_date "%s"' %
+                              (self.feed_end_date, self.feed_start_date))
+
+  def ValidateBeforeAdd(self, problems):
+    transitfeed.ValidateRequiredFieldsAreNotEmpty(self,
+                                                  self._REQUIRED_FIELD_NAMES,
+                                                  problems)
+    self.ValidateFeedInfoLang(problems)
+    self.ValidateFeedInfoPublisherUrl(problems)
+    self.ValidateDates(problems)
+    return True # none of the above validations is blocking
+
+  def ValidateAfterAdd(self, problems):
+    # Validation after add is done in extensions.googletransit.Schedule because
+    # it has to cross check with other files, e.g. feed_lang vs. agency_lang.
+    pass
+
+  def AddToSchedule(self, schedule, problems):
+    schedule.AddFeedInfoObject(self, problems)
