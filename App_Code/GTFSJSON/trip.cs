a3072f521af7699cf2f2a16f8b16d140f75a8e56
kevinsmgov <kevin.hobson@smgov.net>
2015-06-25 14:35:04 -0700

    for trips.txt

---
+++ App_Code/GTFSJSON/trip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace GTFSJSON
+{
+    [DataContract]
+    public class trip
+    {
+        [DataMember]
+        public String route_id { get; set; }
+        [DataMember]
+        public String service_id { get; set; }
+        [DataMember]
+        public String trip_id { get; set; }
+        [DataMember]
+        public String trip_headsign { get; set; }
+        [DataMember]
+        public String trip_short_name { get; set; }
+        [DataMember]
+        public String direction_id { get; set; }
+        [DataMember]
+        public String block_id { get; set; }
+        [DataMember]
+        public String shape_id { get; set; }
+        [DataMember]
+        public String wheelchair_accessible { get; set; }
+        [DataMember]
+        public String bikes_allowed { get; set; }
+        public service service { get; set; }
+    }
+    public class trips : Dictionary<String, trip>
+    {
+
+    }
+}
