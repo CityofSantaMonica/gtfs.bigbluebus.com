2962769372a9c1bf89dd802f7d3d3b1f0d50a62f
Kevin Hobson <Kevin.Hobson@SMGOV.NET>
2020-07-07 08:52:23 -0700

    use Swiftly for alerts as well as trip updates

--- WebApp/App_Data/jobs/triggered/updatetripupdates/run.py
+++ WebApp/App_Data/jobs/triggered/updatetripupdates/run.py
@@ -1,34 +1,8 @@
-﻿import os
-import ssl
-import sys
-import time
-import urllib2
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
-    pass
-else:
-    # Handle target environment that doesn't support HTTPS verification
-    ssl._create_default_https_context = _create_unverified_https_context
+﻿import time
+import updateprotobuf
 
 # take this job 10 seconds out of phase with the transfer from BBB
 time.sleep(10)
 
-agency = os.environ['swiftly_agency']
-key = os.environ['swiftly_key']
-url = 'https://api.goswift.ly/real-time/' + agency + '/gtfs-rt-trip-updates'
-headers = {'Authorization': key}
-
-print(agency)
-print(sys.version)
-print(url)
-
-request = urllib2.Request(url=url,headers=headers)
-response = urllib2.urlopen(request)
-
-f = open("/home/site/wwwroot/tripupdates.bin", "wb")
-content = response.read()
-f.write(content)
-f.close()
+updateprotobuf.update('gtfs-rt-trip-updates', 'tripupdates.bin')
+updateprotobuf.update('gtfs-rt-alerts', 'alerts.bin')
---
+++ WebApp/App_Data/jobs/triggered/updatetripupdates/updateprotobuf.py
@@ -0,0 +1,32 @@
+﻿import os
+import ssl
+import sys
+import time
+import urllib2
+
+def update(apisuffix, targetfile):
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        # Legacy Python that doesn't verify HTTPS certificates by default
+        pass
+    else:
+        # Handle target environment that doesn't support HTTPS verification
+        ssl._create_default_https_context = _create_unverified_https_context
+
+    agency = os.environ['swiftly_agency']
+    key = os.environ['swiftly_key']
+    url = 'https://api.goswift.ly/real-time/' + agency + '/' + apisuffix
+    headers = {'Authorization': key}
+
+    print(agency)
+    print(sys.version)
+    print(url)
+
+    request = urllib2.Request(url=url,headers=headers)
+    response = urllib2.urlopen(request)
+
+    f = open("/home/site/wwwroot/" + targetfile, "wb")
+    content = response.read()
+    f.write(content)
+    f.close()
